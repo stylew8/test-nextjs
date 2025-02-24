89ca26f0a9b65f46208e6d7580d05ba4573a61f9
stylew8 <vaniasavochkin@gmail.com>
2025-02-25 01:01:48 +0200

    lalalal

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/test-nextjs',
+  assetPrefix: '/test-nextjs/',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
